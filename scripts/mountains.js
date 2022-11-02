d42a38eea7c696220042fe0dca3224670d12a728
Sylonjia Odion <Sodion@atl.yearup.org>
2022-11-02 04:45:51 -0400

    new index page & broken mountains page nolonger loading indv. only load all

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,7 +1,13 @@
 let mountains = document.getElementById('mountainData');
 let card = document.getElementById('card');
+const all = document.getElementById("all");
 
 
+function showAllMountains() {
+    mountainsArray.forEach((mountainData) => {
+        onShowCard(card , mountainData)
+    })
+}
 function loadMountainSelection() {
     for (const mountain of mountainsArray) {
         let option = new Option(mountain.name, mountain.name);
@@ -13,20 +19,13 @@ function clearAllCards() {
     card.innerHTML = '';
 }
 
-function onDeleteBtnClick() {
-    const node = document.getElementById("card").firstChild
-    let deleteBtn = document.getElementById('deleteBtn');
-    card.removeChild(node)
-}
-
-
 function onSelectedMountain(event) {
     let selectedVal = event.target.value;
 
     let foundData = mountainsArray.find(item => {
         let mountainData = item.name ?? '';
         if (mountainData?.includes(selectedVal)) {
-            return true;
+            card.appendChild(div);
         }
         return false
     })
@@ -37,22 +36,24 @@ function onSelectedMountain(event) {
 
 }
 
-function onShowCard(mountainData) {
-
-
-
+function onShowCard(cardSection, mountainData) {
     let mounts = mountainData;
-    let div = document.createElement('div')
-    card.appendChild(div)
+    let div = document.createElement('div');
+    cardSection.appendChild(div);
     div.style = "width: 19rem;max-width: 20rem; margin-right: 3rem; border: 7px solid; justify-content: center; margin-bottom: 2rem; padding: 0;"
-    div.display = "flex;";
+    // div.display = "flex;";
     div.className = "col";
     let cardDesc = document.createElement('p');
     let cardName = document.createElement('h3')
     let cardEffort = document.createElement('p');
     let cardEle = document.createElement('p');
+    let eleminateCardBtn = document.createElement('button');
+   
+    const eleminate = () => cardSection.removeChild(div);
+    
+    eleminateCardBtn.onclick = eleminate;
 
-
+    eleminateCardBtn.innerText = "Remove"
     cardName.innerText = "Name: " + mounts.name;
     cardDesc.innerText = "Description: " + mounts.desc;
     cardEffort.innerText = "Effort: " + mounts.effort;
@@ -63,18 +64,20 @@ function onShowCard(mountainData) {
     img.alt = mounts.name ?? '';
     img.style = "width:300px; height:200px"
 
+    div.appendChild(eleminateCardBtn);
     div.appendChild(cardName);
     div.appendChild(img);
     div.appendChild(cardDesc);
     div.appendChild(cardEffort);
     div.appendChild(cardEle);
+    
 }
 
 window.onload = function () {
 
+    all.onclick = showAllMountains;
+
     loadMountainSelection();
-    const deleteBtn = document.getElementById("deleteBtn");
-    deleteBtn.onclick = onDeleteBtnClick;
 
     const clearAllCardsButton = document.getElementById("clearAllCards");
     clearAllCardsButton.onclick = clearAllCards;
